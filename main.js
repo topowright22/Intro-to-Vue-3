739d4277f2524b75d1167f614cf82a8107d7d90c
Lyle Wright <100643029+topowright22@users.noreply.github.com>
2022-07-18 22:48:06 -0700

    Finished second lesson
    Finished second lesson for each of the return elements.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function(){
+        return{
+            product: 'Socks',
+            description: 'The socks are warm and fluffly'
+        }
+    }
+
+})
